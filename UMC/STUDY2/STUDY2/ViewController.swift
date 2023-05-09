4c91c3ae2aa7e3af1c2b11779d94b7a18b502b77
dragosh0705 <pingu0705@hanyang.ac.kr>
2023-05-09 21:48:08 +0900

    study2

---
+++ UMC/STUDY2/STUDY2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  STUDY2
+//
+//  Created by 최영우 on 4/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
