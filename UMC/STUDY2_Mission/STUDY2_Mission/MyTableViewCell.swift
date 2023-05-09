e0d32f3d787b4f0dd7e252a423dcbf68065c5e58
dragosh0705 <pingu0705@hanyang.ac.kr>
2023-05-09 21:51:30 +0900

    study2_mission

---
+++ UMC/STUDY2_Mission/STUDY2_Mission/MyTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyTableViewCell.swift
+//  STUDY2_Mission
+//
+//  Created by 최영우 on 4/7/23.
+//
+
+import UIKit
+
+class MyTableViewCell: NSObject {
+
+}
---
+++ UMC/STUDY2_Mission/STUDY2_Mission/MyTestTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyTestTableViewCell.swift
+//  STUDY2_Mission
+//
+//  Created by 최영우 on 4/7/23.
+//
+
+import UIKit
+
+class MyTestTableViewCell: NSObject {
+
+}
