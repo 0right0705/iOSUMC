9afb204ba31699d8b450fe970928d28b142fe932
dragosh0705 <pingu0705@hanyang.ac.kr>
2023-05-23 14:23:29 +0900

    Study7

---
+++ Desktop/UMC/STUDY7/UserDefaults/UserDefaults/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UserDefaults
+//
+//  Created by 최영우 on 5/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var Login = UITextView = {
+        let login = UITextView()
+        login.translatesAutoresizingMaskIntoConstraints = false
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
