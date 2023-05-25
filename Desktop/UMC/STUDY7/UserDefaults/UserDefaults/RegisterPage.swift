37d1916c0e06bda531cdde7a8a5348122a4272e5
dragosh0705 <pingu0705@hanyang.ac.kr>
2023-05-26 02:32:30 +0900

    Study7

--- Desktop/UMC/STUDY7/UserDefaults/UserDefaults/RegisterPage.swift
+++ Desktop/UMC/STUDY7/UserDefaults/UserDefaults/RegisterPage.swift
@@ -1,17 +1,19 @@
 //
-//  ViewController.swift
+//  RegisterPage.swift
 //  UserDefaults
 //
-//  Created by 최영우 on 5/19/23.
+//  Created by 최영우 on 5/25/23.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class RegisterViewController : UIViewController {
+    
     
     private var LoginLabel : UILabel = {
         let loginlabel = UILabel()
-        loginlabel.text = "ID"
+        loginlabel.text = "사용할 ID"
         loginlabel.textColor = .black
         loginlabel.translatesAutoresizingMaskIntoConstraints = false
         return loginlabel
@@ -28,7 +30,7 @@ class ViewController: UIViewController {
     
     private var passwordLabel : UILabel = {
         let passwordlabel = UILabel()
-        passwordlabel.text = "PassWord"
+        passwordlabel.text = "사용할 PassWord"
         passwordlabel.textColor = .black
         passwordlabel.translatesAutoresizingMaskIntoConstraints = false
         return passwordlabel
@@ -43,42 +45,30 @@ class ViewController: UIViewController {
         return password
     }()
     
-    
-    private var LoginButton : UIButton = {
-        let button = UIButton()
-        button.setTitle("로그인", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
-        button.setTitleColor(.blue, for: .highlighted)
-        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
     private var Register : UIButton = {
         let button = UIButton()
-        button.setTitle("회원가입", for: .normal)
+        button.setTitle("완료", for: .normal)
         button.setTitleColor(.systemOrange, for: .normal)
         button.setTitleColor(.orange, for: .highlighted)
-        button.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+        button
+            .addTarget(self, action: #selector(saveIdAndPasswordInUserDefaults), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
         self.view.addSubview(self.LoginLabel)
         self.view.addSubview(self.Login)
         self.view.addSubview(self.passwordLabel)
         self.view.addSubview(self.Password)
-        self.view.addSubview(self.LoginButton)
         self.view.addSubview(self.Register)
-        
-        Register.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
-
         
         NSLayoutConstraint.activate ([
             self.LoginLabel.heightAnchor.constraint(equalToConstant: 30),
-            self.LoginLabel.widthAnchor.constraint(equalToConstant: 30),
+            self.LoginLabel.widthAnchor.constraint(equalToConstant: 70),
             self.LoginLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200),
             self.LoginLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
         ])
@@ -92,7 +82,7 @@ class ViewController: UIViewController {
         
         NSLayoutConstraint.activate ([
             self.passwordLabel.heightAnchor.constraint(equalToConstant: 30),
-            self.passwordLabel.widthAnchor.constraint(equalToConstant: 80),
+            self.passwordLabel.widthAnchor.constraint(equalToConstant: 140),
             self.passwordLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300),
             self.passwordLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
         ])
@@ -105,50 +95,26 @@ class ViewController: UIViewController {
         ])
         
         NSLayoutConstraint.activate ([
-            self.LoginButton.heightAnchor.constraint(equalToConstant: 30),
-            self.LoginButton.widthAnchor.constraint(equalToConstant: 60),
-            self.LoginButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 400),
-            self.LoginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-        ])
-        
-        NSLayoutConstraint.activate ([
             self.Register.heightAnchor.constraint(equalToConstant: 30),
             self.Register.widthAnchor.constraint(equalToConstant: 80),
-            self.Register.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 450),
+            self.Register.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 400),
             self.Register.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
         ])
     }
     
-    
-    @objc private func registerButtonTapped() {
-        let registerViewController = RegisterViewController()
-        self.present(registerViewController, animated: true)
+    @objc func saveIdAndPasswordInUserDefaults() {
+        guard let id = Login.text else { return }
+        guard let password = Password.text else { return }
+            
+        UserDefaults.standard.setValue(id, forKey: "id")
+        UserDefaults.standard.setValue(password, forKey: "password")
     }
-    
-    @objc private func loginButtonTapped() {
-            guard let enteredID = Login.text, let enteredPassword = Password.text else {
-                // Handle case when ID or password is not entered
-                return
-            }
-            
-            // Retrieve stored ID and password from UserDefaults
-            let storedID = UserDefaults.standard.string(forKey: "id")
-            let storedPassword = UserDefaults.standard.string(forKey: "password")
-            
-            if enteredID == storedID && enteredPassword == storedPassword {
-                // Correct ID and password
-                print("Login successful!")
-            } else {
-                // Incorrect ID or password
-                print("Incorrect ID or password!")
-            }
-        }
-    
-    
-  
-    
-    
 
+    
+    @objc private func didTapButton() {
+        saveIdAndPasswordInUserDefaults()
+        self.dismiss(animated: true, completion: nil)
+    }
 
+    
 }
-
